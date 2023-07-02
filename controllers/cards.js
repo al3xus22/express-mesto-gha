64646d3890be13e796c847612121262c46c1dde1
Aleksey Oleynik <al3xus22@yandex.ru>
2023-07-02 16:05:26 +0300

    Сервер для проекта Место

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,7 +6,7 @@ const createCard = (req, res) => {
 
   Card.create({ name, link, owner })
     .then((card) => {
-      res.send(card);
+      res.status(201).send(card);
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
